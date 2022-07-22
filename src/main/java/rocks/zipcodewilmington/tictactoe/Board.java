71946229c6bef7cc926a0a9b262c28a5057ccd7d
kylehwelch <welch@udel.edu>
2022-07-22 19:27:19 -0400

    finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,26 +1,48 @@
 package rocks.zipcodewilmington.tictactoe;
 
+import java.util.*;
+
 /**
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] board;
+
     public Board(Character[][] matrix) {
+        board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return getWinner().equals("X");
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return getWinner().equals("O");
     }
 
     public Boolean isTie() {
-        return null;
+        return getWinner().equals("");
     }
 
     public String getWinner() {
-        return null;
+        List<String> results = new ArrayList<String>();
+
+        results.add(String.valueOf(board[0][0]) + board [0][1] + board[0][2]);
+        results.add(String.valueOf(board[1][0]) + board [1][1] + board[1][2]);
+        results.add(String.valueOf(board[2][0]) + board [2][1] + board[2][2]);
+
+        results.add(String.valueOf(board[0][0]) + board [1][0] + board[2][0]);
+        results.add(String.valueOf(board[0][1]) + board [1][1] + board[2][1]);
+        results.add(String.valueOf(board[0][2]) + board [1][2] + board[2][2]);
+
+        results.add(String.valueOf(board[0][0]) + board [1][1] + board[2][2]);
+        results.add(String.valueOf(board[0][2]) + board [1][1] + board[2][0]);
+
+        System.out.println(results);
+
+        if (results.contains("XXX")) return "X";
+        else if (results.contains("OOO")) return "O";
+        else return "";
     }
 
 }
